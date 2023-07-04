c93bd75886f64bf90ff9f40d3f26dc78589b6306
Nithis Selvam <nithis.selvam@zucisystems.com>
2023-07-04 12:28:05 +0530

    User1-First-Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthFormComponent } from './auth-form/auth-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'Auth', pathMatch: 'full' },
+  {path:'Auth', component:AuthFormComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
